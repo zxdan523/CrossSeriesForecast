bfaa5e3cc94069b4daf57319522ae88bc97d8ac1
zxdan523 <xzhu08@email.wm.edu>
2020-04-30 08:43:15 -0400

    reset

--- src/Forecast/CIF2016.py
+++
@@ -1,5 +0,0 @@
-from .Util import get_data_from_txt
-
-def import_data(data_path, head_len):
-    data = get_data_from_txt(data_path, head_len)
-    return data
--- src/Forecast/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Util import *
-from .RNN import *
-from .CIF2016 import *
-from .NN5 import *
-from .M5 import *
